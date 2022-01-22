86068e515605feadd712d64fdc2978155486a334
China Hoffman <china.hoffman@gmail.com>
2022-01-22 13:01:42 -0500

    trying out a random reply

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 const { App } = require("@slack/bolt");
 
-//initializes your app with your bot token and signing secret
+//a collection of responses
+const response = [
+  "That sounds hard",
+  "Did you read your error message?",
+  "When I'm stuck, I like to take a break and drink a glass of water",
+  "Why don't you try explaining it to me very slowly?",
+];
+//grab a random response from the responce array
+const randomReply = () => {
+  response[Math.floor(Math.random() * response.length)];
+};
 
+//initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   port: process.env.PORT || 3000,
+});
+
+app.message("hello", async ({ message, say }) => {
+  const sayThis = randomReply();
+  console.log(sayThis);
+  try {
+    await say(sayThis);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 (async () => {
