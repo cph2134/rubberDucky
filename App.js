c52d5fff307b7f4cf701e61dc52f90e96a4bbd36
China Hoffman <china.hoffman@gmail.com>
2022-01-25 11:15:28 -0500

    readme added, code finalized for video

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ const { App } = require("@slack/bolt");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-const store = require("./Home");
 
-const fs = require("fs");
-let raw = fs.readFileSync("db.json");
-let tips = JSON.parse(raw);
 //initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -21,49 +17,37 @@ const app = new App({
   console.log(`Bolt app is running on port ${port}`);
 })();
 
-app.message(/^(hi|hello|hey).*/, async ({ context, say }) => {
+//respond to a greeting
+app.message(/^(hi|hello|hey).*/i, async ({ event, context, say }) => {
   // RegExp matches are inside of context.matches
   const greeting = context.matches[0];
-  // const userId = context.user;
-  console.log(context);
+  const userId = {
+    user: event.user,
+  };
+  let user = await app.client.users.profile.get(userId);
+  let name = user.profile.display_name;
+  console.log(name);
 
-  await say(`${greeting}, how are you?`);
+  await say(`${greeting}, ${name}, how are you?`);
 });
-// if someone sends rubberDucky a DM, they'll get a randomly selected reply from teh responses array
+
 //random responses
 const responses = [
-  "That sounds hard",
   "Did you read your error message?",
-  "When I'm stuck, I like to take a break and drink a glass of water",
   "Why don't you try explaining it to me very slowly?",
   "Did you google your error message?",
+  "Did you really look at your error message?",
   "did you check your terminal?",
   "Maybe you should put in a help ticket",
-  "Maybe you should take a nap",
-  "Are you hungry? Eat a cookie!",
   "Can you draw me a picture?",
 ];
 //grab a random response from the responce array
 const randomReply = () =>
   responses[Math.floor(Math.random() * responses.length)];
 
-// Listens for incoming messages with any content
-// app.message(/.*/, async ({ message, say }) => {
-//   // say() sends a message to the channel where the event was triggered
-//   try {
-//     if (message) {
-//       let response = randomReply();
-//       say(`${response}`);
-//     }
-//   } catch (error) {
-//     console.log("err");
-//     console.error(error);
-//   }
-// });
+//"quack" at the rubberDucky and it will give you a debugging tip!
 
-//if someone uses the "/debugger" command, they'll get a list of resources with descriptions for help with debugging
-
-app.command("/seriously", async ({ command, ack, say }) => {
+app.command("/quack", async ({ ack, say }) => {
   try {
     await ack();
     let message = randomReply();
@@ -75,7 +59,7 @@ app.command("/seriously", async ({ command, ack, say }) => {
 });
 
 //This code determines what appears on the app's home page
-app.event("app_home_opened", async ({ event, client, context }) => {
+app.event("app_home_opened", async ({ event, client }) => {
   // Look up the user from DB
   try {
     const result = await client.views.publish({
@@ -84,12 +68,10 @@ app.event("app_home_opened", async ({ event, client, context }) => {
         type: "home",
         blocks: [
           {
-            type: "header",
-            text: {
-              type: "plain_text",
-              text: "Debugging for beginners",
-              emoji: true,
-            },
+            type: "image",
+            image_url:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiZo_90H2c4xtvV5qkQ9aaPsyTQb3QBXOZzw&usqp=CAU",
+            alt_text: "chart",
           },
           {
             type: "section",
@@ -151,9 +133,8 @@ app.event("app_home_opened", async ({ event, client, context }) => {
           {
             type: "section",
             text: {
-              type: "plain_text",
-              text: "This is a plain text section block.",
-              emoji: true,
+              type: "mrkdwn",
+              text: "<https://www.youtube.com/watch?v=JzpFsrZnNDo&t=0s|A 25-minute Postman primer>",
             },
           },
           {
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 1. create a test workplace
 2. create a new app
+3. Bolt docs <https://slack.dev/bolt-js/concepts/>
 
 **slash commands**
 
